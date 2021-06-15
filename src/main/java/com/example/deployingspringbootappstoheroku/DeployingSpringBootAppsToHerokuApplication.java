2e8e8e0c71d5ca446cb167e53f0688fa8eaa0d44
Wiliam Li <wiliamli.dev@gmail.com>
2021-06-16 04:58:27 +0700

    d

--- src/main/java/com/example/deployingspringbootappstoheroku/DeployingSpringBootAppsToHerokuApplication.java
+++ src/main/java/com/example/deployingspringbootappstoheroku/DeployingSpringBootAppsToHerokuApplication.java
@@ -2,12 +2,21 @@ package com.example.deployingspringbootappstoheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DeployingSpringBootAppsToHerokuApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World! Wiliam here";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeployingSpringBootAppsToHerokuApplication.class, args);
     }
-
 }
