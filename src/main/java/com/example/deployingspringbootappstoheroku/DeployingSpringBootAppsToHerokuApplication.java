ff80c565361ba233b30f7b1bf8dbc794ce4b7497
Wiliam Li <wiliamli.dev@gmail.com>
2021-06-16 04:47:06 +0700

    ok

---
+++ src/main/java/com/example/deployingspringbootappstoheroku/DeployingSpringBootAppsToHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployingspringbootappstoheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployingSpringBootAppsToHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployingSpringBootAppsToHerokuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deployingspringbootappstoheroku/DeployingSpringBootAppsToHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployingspringbootappstoheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployingSpringBootAppsToHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
